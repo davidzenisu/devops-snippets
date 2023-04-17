075eccf8217679d14eb1d3e25fa453b064f86607
David Hofer-Zeni <d.hoferzeni@saa.at>
2023-04-17 16:14:11 +0200

    Added logging to packer task

--- src/container/packer/azure-agent/createImagePacker.sh
+++ src/container/packer/azure-agent/createImagePacker.sh
@@ -1,4 +1,9 @@
-registryCredentials=$(az acr credential show -n XXX -g XXX | jq)
+# $1 ACR registry
+# $2 ACR repository
+# $3 ACR version tag
+
+echo Building container using packer $(packer --version) and pushing to registry $1
+registryCredentials=$(az acr credential show -n $1 -g rg-cq-bicep-sandbox-we | jq)
 registryUsername=$(echo "$registryCredentials" | jq -r '.username')
 registryPassword=$(echo "$registryCredentials" | jq -r '.passwords[0].value')
 
@@ -6,8 +11,8 @@ echo Logging in with $registryUsername
 
 #packer should be installed
 packer build \
-    -var "repositoryName=XXX" \
-    -var "repositoryTag=1.0" \
+    -var "repositoryName=$2" \
+    -var "repositoryTag=$3" \
     -var "registryName=kedatest" \
     -var "registryUsername=$registryUsername" \
     -var "registryPassword=$registryPassword" \
