b05de7d422ea261a658addd3a1141a4386b9dc08
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-16 12:07:26 +0200

    Added scripts to install Notepad++

---
+++ src/auto/notepad/install-notepad-ubuntu.sh
@@ -0,0 +1,8 @@
+# Consider vim, nano, etc. alternatives!
+sudo apt-get install -y snapd 
+sudo snap install snapd
+sudo snap install core
+sudo snap install notepad-plus-plus
+
+# To run, enter:
+# notepad-plus-plus
